17891ef1417cea2ad25bf5074b26133243589019
alhassanhassan99942 <alhassanhassan99942@gmail.com>
2026-01-07 14:25:40 +0100

    Update models.py

--- auth_app/models.py
+++ auth_app/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     date_joined= models.DateTimeField(auto_now_add=True)
     objects = UserManager()
     
-    blood_group = models.CharField(max_length=3, choices=BLOOD_GROUPS, blank=True, null=True)
+    blood_group = models.CharField(max_length=3, choices=BLOOD_GROUPS, blank=True, null=True, default="A+")
     
 
     USERNAME_FIELD = 'email'
