8caa7da336d232fd979dcb42c5814b170ec43bde
alhassanhassan99942 <alhassanhassan99942@gmail.com>
2026-01-07 14:08:08 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -96,10 +96,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "alhassan99942$backend",
+        "USER": "alhassan99942",
+        "PASSWORD": "Ybn99942",
+        "HOST": "alhassan99942.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
     }
 }
 
